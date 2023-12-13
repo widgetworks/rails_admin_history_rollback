71ca0e529472b326d840cd0c0c174d1d762cb123
Nathaniel <nathaniel@widgetworks.com.au>
2023-12-13 17:51:01 +1100

    refactor(assets): remove asset pipline configuration
    RA uses npm package for JS/CSS, use the same for this

--- lib/rails_admin_history_rollback/engine.rb
+++ lib/rails_admin_history_rollback/engine.rb
@@ -1,10 +1,10 @@
 module RailsAdminHistoryRollback
   class Engine < ::Rails::Engine
-    initializer "RailsAdminHistoryRollback precompile hook" do |app|
-      app.config.assets.precompile += [
-        'rails_admin/main.js',
-        'rails_admin/ra.history.css'
-      ]
-    end
+    # initializer "RailsAdminHistoryRollback precompile hook" do |app|
+    #   app.config.assets.precompile += [
+    #     'rails_admin/main.js',
+    #     'rails_admin/ra.history.css'
+    #   ]
+    # end
   end
 end
